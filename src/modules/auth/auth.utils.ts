5d5dd08508d99bfcc7aa908929ea05967d62d216
Md Parvej <mdparvejme24@gmail.com>
2025-07-17 20:59:07 +0600

    update auth utils

--- src/modules/auth/auth.utils.ts
+++ src/modules/auth/auth.utils.ts
@@ -44,15 +44,20 @@ export const generateOtp = () => {
 
 export const sendEmail = async (to: string, subject: string, text: string) => {
   const transporter = nodemailer.createTransport({
-    host: process.env.SMTP_HOST,
-    port: Number(process.env.SMTP_PORT) || 587,
+    // host: process.env.SMTP_HOST,
+    host: "mail.techfynite.com",
+    // port: Number(process.env.SMTP_PORT) || 587,
+    port: 587,
     auth: {
-      user: process.env.SMTP_USER,
-      pass: process.env.SMTP_PASS,
+      user: "parvej@techfynite.com",
+      pass: "123456aA!@#",
+      // user: process.env.SMTP_USER,
+      // pass: process.env.SMTP_PASS,
     },
   });
   await transporter.sendMail({
-    from: process.env.SMTP_USER,
+    from: "parvej@techfynite.com",
+    // from: process.env.SMTP_USER,
     to,
     subject,
     text,
